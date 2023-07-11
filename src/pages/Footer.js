51e51f7ac5811aaee7b1a76cc8c480513c5bc8dc
PRIYA <727721euit116@skcet.ac.in>
2023-07-11 12:19:54 +0530

    Add files via upload

---
+++ src/pages/Footer.js
@@ -0,0 +1,100 @@
+import React from "react";
+import 'login.css';
+export default function Home() {
+  const year = new Date().getFullYear();
+  return (
+    <div >
+      <footer class="footer-section">
+                <div class="container">
+                    <div class="footer-cta pt-5 pb-5">
+                        <div class="row">
+                            <div class="col-xl-4 col-md-4 mb-30">
+                                <div class="single-cta">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                    <div class="cta-text">
+                                        <h4>Find us</h4>
+                                        <span>WXQ5+58F, BK Pudur, Sugunapuram East, Kuniyamuthur, Tamil Nadu 641008</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4 mb-30">
+                                <div class="single-cta">
+                                    <i class="fas fa-phone"></i>
+                                    <div class="cta-text">
+                                        <h4>Call us</h4>
+                                        <span>0422 267 8001</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4 mb-30">
+                                <div class="single-cta">
+                                    <i class="far fa-envelope-open"></i>
+                                    <div class="cta-text">
+                                        <h4>Mail us</h4>
+                                        <span>skcetmail@info.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-content pt-5 pb-5">
+                        <div class="row">
+                            <div class="col-xl-4 col-lg-4 mb-50">
+                               
+                                
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                                <div class="footer-widget">
+                                    <div class="footer-widget-heading">
+                                        <h3>Useful Links</h3>
+                                    </div>
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">about</a></li>
+                                        <li><a href="#">services</a></li>
+                                        <li><a href="#">portfolio</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="#">Our Services</a></li>
+                                        <li><a href="#">Expert Team</a></li>
+                                        <li><a href="#">Contact us</a></li>
+                                        <li><a href="#">Latest News</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                                <div class="footer-widget">
+                                    <div class="footer-widget-heading">
+                                        <h3>Subscribe</h3>
+                                    </div>
+                                    <div class="footer-text mb-25">
+                                        <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                                    </div>
+                                    <div class="subscribe-form">
+                                        <form action="#">
+                                            <input type="text" placeholder="Email Address"></input>
+                                            <button><i class="fab fa-telegram-plane"></i></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                                <div class="copyright-text">
+                                    <p>Copyright &copy; 2018, All Right Reserved <a href=""></a></p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </footer>
+     
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import 'home.css';
+export default function Home() {
+  return (
+    
+    <div className="home">
+     
+      <section class="about-section">
+      <span >WELCOME TO SKCET</span>
+      <h2>We are Creative Tech Enthusiast working since 1998</h2>
+      <div >SKCET is the only chosen institution among all the institution in south India, to jointly offer exclusive B.Tech degree program in Computer Science and Business Systems in partnership with Tata Consultancy Services Ltd. SKCET launched an exclusive M.Tech Computer Science and Engineering (5 Year Integrated) degree program in Full Stack Engineering powered by Virtusa Corporation.</div>
+      
+    </section>
+    </div>
+  );
+}
