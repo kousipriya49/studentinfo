97832996e1903f3849f4caa71ea1e56b77859572
PRIYA <727721euit116@skcet.ac.in>
2023-08-01 10:59:59 +0530

    Delete MINI_PROJECT directory

--- MINI_PROJECT/mini/src/crud/DatabaseConnector.java
+++
@@ -1,9 +0,0 @@
-package crud;
-
-public class DatabaseConnector extends BaseDatabaseConnector {
-    public DatabaseConnector() {
-        super("jdbc:mysql://localhost:3306/mini", "root", "Pr@040903");
-    }
-
-    // You can add additional methods specific to the DatabaseConnector here if needed.
-}
